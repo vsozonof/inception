41a110b43cc9f2e7c4534fa0aa6c945e30043401
Your Name <you@example.com>
2025-04-01 09:01:38 -0400

    inception finished

--- srcs/wordpress/conf/auto_config.sh
+++ srcs/wordpress/conf/auto_config.sh
@@ -10,6 +10,7 @@ until mysql -hmariadb -u$SQL_USER -p$SQL_PASSWORD -e "SHOW DATABASES;" > /dev/nu
 done
 echo "MariaDB is ready!"
 
+echo "Creating  the WP config!"
 if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
     wp-cli.phar config create --allow-root \
         --dbname=$SQL_DATABASE \
@@ -18,12 +19,13 @@ if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
         --dbhost=mariadb:3306 --path='/var/www/wordpress'
 fi
 
+echo "Creating the WP core and $WP_USER user!"
 if ! wp-cli.phar core is-installed --allow-root --path='/var/www/wordpress'; then
 	wp-cli.phar core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_USER" --admin_password="$WP_PASSWORD" --admin_email="$WP_EMAIL" --path='/var/www/wordpress' --allow-root
 fi
 
+echo "Creating user with login: $WP_USER2, email: $WP_EMAIL2"
 if ! wp-cli.phar user get $WP_USER2 --allow-root --path='/var/www/wordpress'; then
-	echo "Creating user with login: $WP_USER2, email: $WP_EMAIL2"
     wp-cli.phar user create $WP_USER2 $WP_EMAIL2 --user_pass=$WP_PASSWORD2 --role=administrator --path='/var/www/wordpress' --allow-root
 fi
 
